e8449858d483c7e8f9b43f375e385a6e3719cfe0
Iconstudio <yoyofa2@hotmail.com>
2023-05-04 17:13:28 +0900

    Visual Studio Solution Initialized

---
+++ RefinedServer/RefinedServer/pch.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __PCH__
+#define __PCH__
+
+#include "stdafx.hpp"
+
+#endif // __PCH__
---
+++ RefinedServer/RefinedServer/stdafx.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <sdkddkver.h>
+
+#include <utility>
+#include <iostream>
+
+#include <string>
+#include <string_view>
+#include <format>
+
+#include <algorithm>
+#include <ranges>
