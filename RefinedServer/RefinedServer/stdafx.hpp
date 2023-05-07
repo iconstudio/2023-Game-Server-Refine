edd0c318fd08134ef47e063bfbd2ce69f627f1bf
Iconer <yoyofa2@hotmail.com>
2023-05-08 05:26:21 +0900

    정적 라이브러리 참조 추가

--- RefinedServer/RefinedServer/stdafx.hpp
+++ RefinedServer/RefinedServer/stdafx.hpp
@@ -1,4 +1,20 @@
 #pragma once
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "mswsock.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "crypt32.lib")
+#pragma comment(lib, "userenv.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "winspool.lib")
+#pragma comment(lib, "comdlg32.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "ole32.lib")
+
 #include <sdkddkver.h>
 
 #include <utility>
