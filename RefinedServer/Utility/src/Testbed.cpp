12eb5e4fe7a152c13073a2535f9fb0abef3cdaaf
Iconer <yoyofa2@hotmail.com>
2023-05-12 22:55:57 +0900

    C++ 23

---
+++ RefinedServer/Utility/src/Testbed.cpp
@@ -0,0 +1,14 @@
+#pragma once
+import Utility;
+import Utility.Constraints;
+import Utility.Identity;
+import Utility.Monad;
+import Utility.Monad.Loosen;
+import Utility.Print;
+
+int main()
+{
+	util::Println("Hello, world!");
+
+	return 0;
+}
