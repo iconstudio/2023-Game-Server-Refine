6857c5411de6889770e059d32eed5b5a71e69f72
Iconer <yoyofa2@hotmail.com>
2023-05-15 01:55:45 +0900

    수용 버그 수정

--- RefinedServer/RefinedServer/Framework.cpp
+++ RefinedServer/RefinedServer/Framework.cpp
@@ -22,7 +22,7 @@ public:
 		: myMsg(msg)
 	{}
 
-	void operator()(int&& error_code)
+	inline void operator()(int&& error_code)
 	{
 		util::Println("{} (오류 코드: {})", myMsg, error_code);
 		util::err::RaiseSystemError(std::move(error_code));
@@ -57,7 +57,7 @@ void Framework::Awake()
 	}).else_then(OnError("서버의 TCP 소켓을 생성하는데 실패했습니다."));
 
 	nameEndPoint = EndPoint::CreateStaticTCP(AddressFamily::IPv4, serverPort);
-	nameSocket.Bind(nameEndPoint).else_then(OnError("서버의 UDP 소켓을 생성하는데 실패했습니다."));
+	nameSocket.Bind(nameEndPoint).else_then(OnError("서버의 주소를 지정하는데 실패했습니다."));
 
 	Socket::CreateUDP().if_then([this](Socket&& socket) noexcept {
 		gameSocket = std::move(socket);
@@ -67,13 +67,10 @@ void Framework::Awake()
 		util::Println("서버의 수용을 시작합니다.");
 	}).else_then(OnError("서버의 수용 시작 단계가 실패했습니다."));
 
-	nameSocket.Accept(gameSocket, acceptBuffer, acceptContext, accceptResultSize).else_then(
-		[](int&& error_code) {
-		util::Println("서버의 첫번째 비동기 수용 단계가 실패했습니다. 오류 코드: {}", error_code);
-		util::err::RaiseSystemError(std::move(error_code));
+	auto test_socket = Socket::CreateTCP();
 
-		return;
-	});
+	nameSocket.Accept(*test_socket.GetResult(), acceptBuffer, acceptContext, accceptResultSize)
+		.else_then(OnError("첫번째 비동기 수용 단계가 실패했습니다."));
 }
 
 void Framework::Start() noexcept
