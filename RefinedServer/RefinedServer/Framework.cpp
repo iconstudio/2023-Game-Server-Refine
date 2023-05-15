602e2d8bc3c8743576b2630d99646e676711585b
Iconer <yoyofa2@hotmail.com>
2023-05-16 04:40:11 +0900

    IOCP와 서버 소켓 연결

--- RefinedServer/RefinedServer/Framework.cpp
+++ RefinedServer/RefinedServer/Framework.cpp
@@ -54,10 +54,15 @@ void Framework::Awake()
 
 	Socket::CreateTCP().if_then([this](Socket&& socket) noexcept {
 		nameSocket = std::move(socket);
-	}).else_then(OnError("서버의 TCP 소켓을 생성하는데 실패했습니다."));
+	})
+		.else_then(OnError("서버의 TCP 소켓을 생성하는데 실패했습니다."));
 
 	nameEndPoint = EndPoint::CreateStaticTCP(AddressFamily::IPv4, tcpPort);
-	nameSocket.Bind(nameEndPoint).else_then(OnError("서버의 주소를 지정하는데 실패했습니다."));
+	nameSocket.Bind(nameEndPoint)
+		.else_then(OnError("서버의 주소를 지정하는데 실패했습니다."));
+
+	ioPort.Link(nameSocket.Handle(), 0ULL)
+		.else_then(OnError("서버의 TCP 소켓을 IOCP에 연결하는데 실패했습니다."));
 
 	Socket::CreateUDP().if_then([this](Socket&& socket) noexcept {
 		gameSocket = std::move(socket);
@@ -70,6 +75,9 @@ void Framework::Awake()
 
 	gameEndPoint = EndPoint::CreateStaticUDP(AddressFamily::IPv4, udpPort);
 	gameSocket.Bind(gameEndPoint).else_then(OnError("서버의 UDP 주소를 지정하는데 실패했습니다."));
+
+	ioPort.Link(gameSocket.Handle(), 1ULL)
+		.else_then(OnError("서버의 UDP 소켓을 IOCP에 연결하는데 실패했습니다."));
 
 	gameSocket.optDebug = true;
 	gameSocket.optReuseAddress = true;
