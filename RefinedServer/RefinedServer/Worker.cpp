9b3515677c33db0effa3c96d03aa452f02d6e619
Iconstudio <yoyofa2@hotmail.com>
2023-05-17 17:01:18 +0900

    작업자 소스 파일 추가

---
+++ RefinedServer/RefinedServer/Worker.cpp
@@ -0,0 +1,8 @@
+#include "pch.hpp"
+#include "Worker.hpp"
+#include "Framework.hpp"
+
+void Worker(util::CancellationToken cancel_token, net::CompletionPort& completion_port)
+{
+
+}
---
+++ RefinedServer/RefinedServer/Worker.hpp
@@ -0,0 +1,6 @@
+#pragma once
+import Utility.Concurrency.Thread;
+import Net;
+
+void Worker(util::CancellationToken cancel_token, net::CompletionPort& completion_port);
+
