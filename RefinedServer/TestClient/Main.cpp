617132aa79a122a174f24d57e7739003ce140f6e
Iconer <yoyofa2@hotmail.com>
2023-05-22 03:10:11 +0900

    테스트 OpenGL 프로젝트 초기화

---
+++ RefinedServer/TestClient/Main.cpp
@@ -0,0 +1,6 @@
+#include "Main.hpp"
+
+int main()
+{
+	return 0;
+}
---
+++ RefinedServer/TestClient/Main.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
+
+class Main
+{};
+
