180b9f836073862f05029bd4971a5cd6cc5e69c6
Iconer <yoyofa2@hotmail.com>
2023-05-23 11:27:06 +0900

    테스트 클라 수정

--- RefinedServer/TestClient/System.hpp
+++ RefinedServer/TestClient/System.hpp
@@ -1,4 +1,25 @@
 #pragma once
-class System
-{};
 
+// @brief Class representing a camera orbiting around its target.
+// @ingroup gl_utils
+class System
+{
+public:
+	System();
+	~System();
+
+	void Update(float dt);
+
+	void SetCameraTarget(const glm::vec3& target);
+	void SetCameraDistance(const float& distance);
+	void SetCameraRotation(const float& rotation);
+	void SetCameraElevation(const float& z);
+
+	const glm::vec3& GetTarget() const;
+	float GetDistance() const;
+	float GetRotation() const;
+	float GetElevation() const;
+
+	glm::vec3 getPosition() const;
+	glm::mat4 getViewMatrix() const;
+};
--- RefinedServer/TestClient/pch.hpp
+++ RefinedServer/TestClient/pch.hpp
@@ -3,3 +3,9 @@
 #include <GL/GLU.h>
 #include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+import Net;
+import Net.Socket;
+import Net.EndPoint;
