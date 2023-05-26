caa77fdbdb3908d83f1ca6dbb91360fc213107e2
Iconer <yoyofa2@hotmail.com>
2023-05-26 23:54:58 +0900

    테스트 코드 삭제

--- RefinedServer/RefinedServer/main.cpp
+++ RefinedServer/RefinedServer/main.cpp
@@ -1,15 +1,12 @@
 ﻿#include "pch.hpp"
 #include "Framework.hpp"
 //#include "../Utility/Testbed.h"
-import Utility.Datagram.Packer;
 
 Framework* myFramework;
 
 int main()
 {
 	myFramework = new Framework{};
-
-	util::test::test_datapacker();
 
 	myFramework->Awake();
 	myFramework->Start();
