2d32e043df2aa9e17cd81d5b2a9447b0eab61034
Iconer <yoyofa2@hotmail.com>
2023-05-29 17:36:47 +0900

    메인 씬 수정

--- RefinedServer/TestClient/MainScene.hpp
+++ RefinedServer/TestClient/MainScene.hpp
@@ -3,13 +3,13 @@ import Net;
 import Net.Socket;
 import Net.EndPoint;
 import Net.Promise;
-import Game.Scene;
+import Game.Scene.Specialized;
 
-class MainScene final : public game::Scene
+class MainScene final : public game::NamedScene<MainScene>
 {
 public:
 	MainScene() noexcept
-		: Scene()
+		: NamedScene<MainScene>(this, "Main Scene")
 		, mySocket()
 		, myAddress()
 	{}
