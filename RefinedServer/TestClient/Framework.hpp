29c7159be1855e24bdb0ba119b38638a2775e55a
Iconer <yoyofa2@hotmail.com>
2023-06-01 05:09:27 +0900

    헤테로지니어스 기반의 클라이언트를 위해 작업 함수와 포인터 자료형 정의

--- RefinedServer/TestClient/Framework.hpp
+++ RefinedServer/TestClient/Framework.hpp
@@ -7,11 +7,18 @@ import Utility;
 import Utility.Singleton;
 import Utility.Constraints;
 import Utility.Monad;
+import Utility.Concurrency.Thread;
 import Game.Camera;
 import Game.GameObject;
 import Game.Scene;
 import Game.Scene.Specialized;
 import Game.Scene.Management;
+
+class Framework;
+using Hetero = std::unique_ptr<util::jthread>;
+void GameUpdater(Framework& system);
+void GameInputs(Framework& system);
+void GameRenderer(Framework& system);
 
 class Framework : public util::Singleton<Framework>
 {
@@ -28,4 +35,6 @@ public:
 
 	void UpdateOnce(game::Scene* const& scene, const float delta_time);
 	void LateUpdateOnce(game::Scene* const& scene, const float delta_time);
+
+	Hetero myWorkUnit = nullptr, myInputUnit = nullptr, myRenderUnit = nullptr;
 };
