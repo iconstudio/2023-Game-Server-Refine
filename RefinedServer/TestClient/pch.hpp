902f838a830c347dbada89b048dc373c0037ec9c
Iconer <yoyofa2@hotmail.com>
2023-06-02 20:27:53 +0900

    PCH에서 삽입하던 헤더 삭제

--- RefinedServer/TestClient/pch.hpp
+++ RefinedServer/TestClient/pch.hpp
@@ -1,15 +1,3 @@
 #pragma once
-#include <GL/freeglut.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-
-import <utility>;
-import <memory>;
-import <thread>;
-import <vector>;
-import <algorithm>;
-import <ranges>;
-
 #define LIKELY [[likely]]
 #define UNLIKELY [[unlikely]]
