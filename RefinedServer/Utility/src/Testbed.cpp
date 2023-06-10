cdbf2f66304372fc062ec90155806d049191b130
Iconer <yoyofa2@hotmail.com>
2023-06-10 10:08:23 +0900

    테스트 코드 비활성화

--- RefinedServer/Utility/src/Testbed.cpp
+++ RefinedServer/Utility/src/Testbed.cpp
@@ -1,11 +1,16 @@
 #include <WS2tcpip.h>
-#include <MSWSock.h>
+#include <charconv>
 
-import Utility;
-import Utility.Option;
-import Net;
-import Net.Addressing;
-import Net.EndPoint;
-import Net.Context;
-import Net.Promise;
+#if __TEST__
+int main()
+{
+	constexpr char str[] = "123123";
 
+	short v_i16;
+	unsigned short v_u16;
+	int v_i32;
+	unsigned v_u32n;
+
+	std::from_chars(str, str + 7, v_i16); // (1)
+}
+#endif
