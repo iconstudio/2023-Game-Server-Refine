334b0b46f81f6e189b72ad4c0c93c4cf7c16a1eb
Iconer <yoyofa2@hotmail.com>
2023-06-12 04:19:29 +0900

    현재 <chrono> 모듈에 기묘한 인텔리센스 버그가 있음

--- RefinedServer/TestClient/Framework.cpp
+++ RefinedServer/TestClient/Framework.cpp
@@ -4,14 +4,12 @@ module;
 
 module Client.Framework;
 import Utility.Print;
-import Game.Scene.Management;
 import Client.IntroScene;
 import Client.TitleScene;
 import Client.MainScene;
 import Client.GameScene;
 
 using namespace ::game;
-using namespace ::std::chrono_literals;
 
 void Framework::Awake()
 {
@@ -26,105 +24,43 @@ void Framework::Awake()
 	SceneManager::SetActiveScene(0);
 }
 
-void Framework::Start() noexcept
+void Framework::UpdateOnce(Scene* const& scene)
 {
-	util::Println("게임을 준비합니다.");
+	auto start_time = std::chrono::high_resolution_clock::now();
+	/*
+	start_time = clock::now();
 
-	if (Scene* const& intro = SceneManager::GetActiveScene(); intro != nullptr)
+	scene->Update(est);
+	util::debug::Println("씬 업데이트 (시간: {:.5f}초)", est);
+
+	current_time = clock::now();
+	between = current_time - start_time;
+	est = static_cast<float>(between.count()) * idv;
+	stacked_time += std::chrono::duration_cast<alter_duration>(between);
+
+	if (scene->IsCompleted())
 	{
-		util::debug::Println("첫번째 씬으로 '{}'에서 게임을 시작합니다.", *intro);
-		intro->Start();
-	}
-	else
-	{
-		util::Println("첫번째 씬이 없어서 게임을 시작할 수 없습니다.");
-	}
-}
-
-void Framework::Update()
-{
-	util::Println("프레임워크의 갱신 루프를 시작합니다.");
-
-	using clock = std::chrono::steady_clock;
-	using time_point = clock::time_point;
-	using duration = clock::duration;
-	using period = clock::period;
-	using alter_duration = std::chrono::milliseconds;
-	constexpr float num = static_cast<float>(period::num);
-	constexpr float den = static_cast<float>(period::den);
-	constexpr float idv = num / den;
-
-	time_point start_time{};
-	time_point current_time{};
-	duration between{};
-	float est = 0;
-
-	alter_duration stacked_time{};
-
-	while (true)
-	{
-		Scene* const& scene = SceneManager::GetActiveScene();
-		if (scene == nullptr) UNLIKELY
+		if (!game::SceneManager::IsAvailableNext())
 		{
-			util::Println("현재 가리키는 씬이 없습니다.");
+			util::Println("다음 씬이 없습니다.");
 			break;
-		};
-
-		if (scene->IsPaused()) UNLIKELY
+		}
+		else
 		{
-			std::this_thread::yield();
-			std::this_thread::sleep_for(10ms);
-			continue;
-		};
+			const util::Monad<game::Scene*> scene_wrapper = game::SceneManager::GotoNextScene();
 
-		start_time = clock::now();
-
-		UpdateOnce(scene, est);
-		util::debug::Println("씬 업데이트 (시간: {:.5f}초)", est);
-
-		current_time = clock::now();
-		between = current_time - start_time;
-		est = static_cast<float>(between.count()) * idv;
-		stacked_time += std::chrono::duration_cast<alter_duration>(between);
-
-		if (scene->IsCompleted()) UNLIKELY
-		{
-			if (!game::SceneManager::IsAvailableNext()) UNLIKELY
+			if (scene_wrapper.has_value())
 			{
-				util::Println("다음 씬이 없습니다.");
-				break;
+				game::Scene* const& next = *scene_wrapper;
+
+				util::Println("다음 씬 {}을(를) 시작합니다.", *next);
+				next->Start();
 			}
 			else
 			{
-				const util::Monad<game::Scene*> scene_wrapper = SceneManager::GotoNextScene();
-
-				if (scene_wrapper.has_value())
-				{
-					Scene* const& next = *scene_wrapper;
-
-					util::Println("다음 씬 {}을(를) 시작합니다.", *next);
-					next->Start();
-				}
-				else
-				{
-					util::Println("오류! 씬 {}에 문제가 있습니다.", *scene);
-					break;
-				}
-			};
-		};
-
-#if _DEBUG
-		//std::this_thread::sleep_for(3000ms);
-#endif
-	}
-}
-
-void Framework::UpdateOnce(Scene* const& scene, const float delta_time)
-{
-	scene->Update(delta_time);
-}
-
-void Framework::Render(Scene* const& scene, const void* context) noexcept
-{
-	scene->Render(context);
+				util::Println("오류! 씬 {}에 문제가 있습니다.", *scene);
+				break;
+			}
+		}
+	}*/
 }
--- RefinedServer/TestClient/Framework.ixx
+++ RefinedServer/TestClient/Framework.ixx
@@ -28,10 +28,51 @@ public:
 	constexpr ~Framework() noexcept = default;
 
 	void Awake();
-	void Start() noexcept;
-	void Update();
-	void UpdateOnce(game::Scene* const& scene, const float delta_time);
-	void Render(game::Scene* const& scene, const void* context) noexcept;
+
+	void Start() noexcept
+	{
+		util::Println("게임을 준비합니다.");
+
+		if (game::Scene* const& intro = game::SceneManager::GetActiveScene(); intro != nullptr)
+		{
+			util::debug::Println("첫번째 씬으로 '{}'에서 게임을 시작합니다.", *intro);
+			intro->Start();
+		}
+		else
+		{
+			util::Println("첫번째 씬이 없어서 게임을 시작할 수 없습니다.");
+		}
+	}
+
+	void Update()
+	{
+		util::Println("프레임워크의 갱신 루프를 시작합니다.");
+
+		while (true)
+		{
+			game::Scene* const& scene = game::SceneManager::GetActiveScene();
+			if (scene == nullptr)
+			{
+				util::Println("현재 가리키는 씬이 없습니다.");
+				break;
+			};
+
+			if (scene->IsPaused())
+			{
+				util::this_thread::yield();
+				continue;
+			};
+
+			UpdateOnce(scene);
+		}
+	}
+
+	void UpdateOnce(game::Scene* const& scene);
+
+	void Render(game::Scene* const& scene, const void* context) noexcept
+	{
+		scene->Render(context);
+	}
 
 	Hetero myWorkUnit = nullptr, myInputUnit = nullptr, myRenderUnit = nullptr;
 
