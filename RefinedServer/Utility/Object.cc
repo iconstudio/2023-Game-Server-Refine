44554ba031a647bb18c8e63d0f42bed7aa45e821
Iconer <yoyofa2@hotmail.com>
2023-06-13 05:35:35 +0900

    모듈에는 리플렉션 적용이 아직 안됨

--- RefinedServer/Utility/Object.cc
+++ RefinedServer/Utility/Object.cc
@@ -1,6 +1,9 @@
+#include "Reflection.cc"
+
 import <string>;
 import <string_view>;
 import <vector>;
+import Utility;
 import Utility.Constraints;
 import Utility.Classifier;
 import Utility.Meta;
@@ -12,31 +15,39 @@ namespace util
 	/// <summary>
 	/// Reflected base class for all objects.
 	/// </summary>
-	class Reflection :
+	class Object :
 		public Named,
-		public Classifier<Reflection>,
-		public Indexer<Reflection>
+		public Classifier<Object>,
+		public Indexer<Object>
 	{
 	public:
-		constexpr Reflection() noexcept = default;
-		constexpr ~Reflection() noexcept = default;
+		constexpr Object() noexcept = default;
+		constexpr ~Object() noexcept = default;
 
 		template<typename Self>
-		consteval std::string_view GetName(this Self&& self) noexcept
+		[[nodiscard]]
+		constexpr Self&& GetClass(this Self&& self) noexcept
 		{
-
+			//static_assert(always_false<Self>());
+			return forward<Self>(self);
 		}
 
+		template<typename Self>
+		[[nodiscard]]
+		constexpr std::string_view GetName(this Self&& self) noexcept
+		{
+			GetClass().GetName();
+		}
 
-		constexpr Reflection(const Reflection& other) noexcept = default;
-		constexpr Reflection& operator=(const Reflection& other) noexcept = default;
-		constexpr Reflection(Reflection&& other) noexcept = default;
-		constexpr Reflection& operator=(Reflection&& other) noexcept = default;
+		constexpr Object(const Object& other) noexcept = default;
+		constexpr Object& operator=(const Object& other) noexcept = default;
+		constexpr Object(Object&& other) noexcept = default;
+		constexpr Object& operator=(Object&& other) noexcept = default;
 	};
 }
 
 template<>
-struct util::class_id<util::Reflection>
+struct util::class_id<util::Object>
 {
 	static constexpr size_t value = static_cast<size_t>(-1);
 };
